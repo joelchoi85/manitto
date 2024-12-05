1c2998f1c7b28ea7490ee4d6c49be31d6b8c9eb7
joelchoi85 <jymania@naver.com>
2024-12-06 02:54:33 +0900

    react19 zustand 미호환으로 overrides, /manitto 페이지 window not defined 오류 수정시도

--- app/manitto/page.tsx
+++ app/manitto/page.tsx
@@ -1,7 +1,7 @@
 import ManittoMain from '@/components/manitto-main';
 // import { createClient } from '@/lib/server/supabase';
 // import { Member } from '@prisma/client';
-export default async function Manitto() {
+export default function Manitto() {
 	// const supabase = await createClient();
 	// const { data } = await supabase.from('Member').select();
 	return <ManittoMain />;
